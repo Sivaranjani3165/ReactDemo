e36e15b7b2dff34a2fb12239916781597a108191
Sivaranjani <sivaranjani.kmrj@gmail.com>
2023-04-22 04:20:30 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const h1 = React.createElement("h1",{},'Hello Sivaranjani!');
+const span = React.createElement("span",{},'Restart and Retune your Life');
+const div = React.createElement("div",{},[h1,span]);
+root.render(div);
